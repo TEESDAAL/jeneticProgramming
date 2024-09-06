0d13658c0574b37a05c5d1abb7f94d44b1517163
Alan Teesdale <78932086+TEESDAAL@users.noreply.github.com>
2024-09-06 23:24:53 +1200

    added parallelisation

--- GeneticProgramming.java
+++ GeneticProgramming.java
@@ -1,6 +1,7 @@
 import com.sun.source.tree.Tree;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 
@@ -15,13 +16,13 @@ public class GeneticProgramming {
     private static List<Double> populationAverages = new ArrayList<>();
     static class ErrorFunctions {
         static <E extends Expression<E>> Function<Expression<E>, Double> absoluteError(List<Double> xValues, Function<Double, Double> groundTruth) {
-            return model -> xValues.stream()
+            return model -> xValues.parallelStream()
                     .mapToDouble(x -> model.evaluate(x) - groundTruth.apply(x))
                     .map(Math::abs)
                     .sum();
         }
         static <E extends Expression<E>> Function<Expression<E>, Double> meanSquaredError(List<Double> xValues, Function<Double, Double> groundTruth) {
-            return model -> xValues.stream()
+            return model -> xValues.parallelStream()
                     .mapToDouble(x -> model.evaluate(x) - groundTruth.apply(x))
                     .map(error -> Math.pow(error, 2))
                     .sum();
@@ -32,7 +33,7 @@ public class GeneticProgramming {
     final static double CROSS_OVER_RATE = 0.8;
     final static double MUTATION_RATE = 0.1;
 
-    private final static int POPULATION_SIZE = 100;
+    private final static int POPULATION_SIZE = 1000;
 
     static <U> U choose(List<U> list) {
         assert !list.isEmpty();
@@ -47,17 +48,22 @@ public class GeneticProgramming {
 
         Function<Double, Double> function = x -> Math.pow(x, 3) + 2*Math.pow(x, 2) - 7*x + 3;
         List<Double> points = pointsWithinRange(-5, 5, 500);
+        int epochs = 100;
 
-        for (int i=1; i<=100; i++) {
-            System.out.println("\nPopulation statistics for generation: " + i);
-            populationStatistics(population, ErrorFunctions.absoluteError(points, function));
+        for (int i=1; i<=epochs; i++) {
+            if (i % 10 == 0) {
+                System.out.println("\nPopulation statistics for generation: " + i);
+                populationStatistics(population, ErrorFunctions.absoluteError(points, function));
+            }
             population = tournamentSelection(population, ErrorFunctions.absoluteError(points, function), TOURNAMENT_SIZE, POPULATION_SIZE / 2);
             System.out.println("Selected "+ population.size()+ " programs");
             population = breed(population, POPULATION_SIZE);
             System.out.println("Bred a new generation of "+ population.size()+ " programs");
         }
         System.out.println("Average Progression: " + populationAverages);
+
     }
+
 
     static List<Double> pointsWithinRange(int lowerBound, int upperBound, int numberOfPoints) {
         return IntStream.range(0, numberOfPoints).mapToObj(i -> lowerBound + upperBound/ (double) numberOfPoints).toList();
@@ -69,14 +75,14 @@ public class GeneticProgramming {
             return;
         }
 
-        E bestProgram = population.stream()
+        E bestProgram = population.parallelStream()
                 .min(Comparator.comparingDouble(fitnessFunction::apply))
                 .get();
 
         System.out.println("The most accurate model : "+bestProgram+"     ("+fitnessFunction.apply(bestProgram)+")");
 
         populationAverages.add(
-                population.stream()
+                population.parallelStream()
                 .mapToDouble(fitnessFunction::apply)
                 .sum() / population.size()
         );
@@ -84,43 +90,47 @@ public class GeneticProgramming {
         System.out.println("Average Population fitness: " + populationAverages.getLast());
     }
 
-    public static <E extends Expression<E>> List<E> tournamentSelection(List<E> population, Function<Expression<E>, Double> fitnessFunction, int tournamentSize, int numberToSelect) {
+    private static <E extends Expression<E>> List<E> tournamentSelection(List<E> population, Function<Expression<E>, Double> fitnessFunction, int tournamentSize, int numberToSelect) {
         population = new ArrayList<>(population);
         assert tournamentSize >= 1 : "Must try and select at least one individual.";
         if (population.isEmpty() || numberToSelect == 0) {return List.of();}
 
-        Collections.shuffle(population);
-        List<E> selectedPopulation = new ArrayList<>();
-        while (selectedPopulation.size() < numberToSelect) {
-            // Sample with replacement
-            var bestExpression = population.stream()
-                    .limit(tournamentSize)
-                    .min(Comparator.comparingDouble(fitnessFunction::apply))
-                    .orElseThrow(IllegalStateException::new);
-            selectedPopulation.add(bestExpression);
-        }
+        List<E> finalPopulation = population;
 
-        return selectedPopulation;
+        return IntStream.range(0, numberToSelect)
+                .parallel()
+                .mapToObj(i -> {
+                    // Sample with replacement
+                     return ThreadLocalRandom.current()
+                            .ints(0, finalPopulation.size())
+                            .distinct()
+                            .limit(tournamentSize)
+                            .mapToObj(finalPopulation::get)
+                            .min(Comparator.comparingDouble(fitnessFunction::apply))
+                            .get();
+                })
+                .toList();
     }
 
     static <E extends Expression<E>> List<E> breed(List<E> population, int desiredSize) {
-        List<E> newPopulation = new ArrayList<>();
-        int i = 0;
-        while (newPopulation.size() < desiredSize) {
-            i = (i+1) % population.size();
-            double p = Math.random();
-            var e = population.get(i).copy();
 
-            if (p < CROSS_OVER_RATE) {
-                newPopulation.add(e.crossOver(choose(population).copy()));
-            } else if (p < CROSS_OVER_RATE + MUTATION_RATE) {
-                newPopulation.add(e.mutate());
-            } else {
-                newPopulation.add(e);
-            }
-        }
-        return newPopulation;
+        return IntStream.range(0, desiredSize).parallel()
+                .map(i -> i % population.size())
+                .mapToObj(i -> {
+                            double p = Math.random();
+                            var e = population.get(i).copy();
+
+                            if (p < CROSS_OVER_RATE) {
+                                return e.crossOver(choose(population).copy());
+                            } else if (p < CROSS_OVER_RATE + MUTATION_RATE) {
+                                return e.mutate();
+                            } else {
+                                // Reproduction
+                                return e;
+                            }
+                        }
+                )
+                .toList();
     }
-
 }
 
